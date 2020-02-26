0913832f2a3149b97c5968c4d73b359176e5c915
sabrinaywoo <48927136+sabrinaywoo@users.noreply.github.com>
2020-02-26 10:19:59 -0700

    Update SimpleList.java

--- SimpleList.java
+++ SimpleList.java
@@ -1,9 +1,8 @@
 /**
  *Name: Sabrina Woo
  *Class ID: 422
- *Assignment 1
- *Description: Simple List class can add remove or 
- *					search an integer
+ *Assignment 2
+ *Description: Simple List class can add, remove, or search an integer
 **/
 
 public class SimpleList {
@@ -26,34 +25,57 @@ public int [] getList()
 
 //add a new int to array at index 0
 public void add (int newInt)
-{
-	if (count  == 0)
+{	
+	//if list is empty then add an integer		
+	if (count == 0)
 	{
 		list[0] = newInt;
 		count++;
 	}
+	//check if list is full
+	else if (count == list.length)
+	{
+		//new array increases size by 50%
+		int [] tempArray = new int [(int)(list.length * .5)];
+		
+		//copy original list elements into new array
+		for (int copy = 0; copy < list.length; copy++)
+		{
+			tempArray[copy] = list[copy];
+		}
+		list = tempArray;
+		add(newInt);
+	}
+	//add a new int
 	else
 	{
+		//shift integers to the right
 		for (int index = list.length - 1; index > 0; index --)
 		{
 			list[index] = list [index - 1]; 
 		}
+		//add int at beginning of the list and increment count
 		list[0] = newInt;
 		count++;
 	}
 
 }
 
+//remove a specified int
 public void remove (int target)
 {
-	int secondIndex, firstIndex;
+	//list search variables
+	int firstIndex, secondIndex;
+	//75% size of list
+	int reducedSize = (int)(list.length * 0.75);
+	
 	for (firstIndex = 0; firstIndex < list.length; firstIndex ++)
 	{
 		if (list[firstIndex] == target)
 		{
-			if(list[list.length-1] == target)
+			if(list[list.length - 1] == target)
 			{
-				list[list.length-1] = 0;
+				list[list.length - 1] = 0;
 				//reduce count since a number was removed
 				count --;
 			}
@@ -71,6 +93,20 @@ public void remove (int target)
 			}
 			
 		}
+	}
+	
+	//reduce array size if over 25% of list is empty
+	if (count < reducedSize)
+	{
+		//new array with smaller size
+		int [] tempArray = new int [reducecdSize];
+		
+		//copy original list elements into new array
+		for (int copy = 0; copy < reducedSize; copy++)
+		{
+			tempArray[copy] = list[copy];
+		}
+		list = tempArray;
 	}
 }
 
